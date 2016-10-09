6d6a01497ba9de78e7337c22eb6792c8f952ce0e
Luke <lkwakabayashi@gmail.com>
2016-10-09 16:58:07 -0400

    test commit

--- src/Chapter.java
+++ src/Chapter.java
@@ -3,6 +3,27 @@
  */
 public abstract class Chapter {
 
-    public Chapter()
+    public static void main(String[] args)
+    {
+        for(int i = 1; i<=100; i++)
+        {
+            if (i % 5 == 0 && i % 3 == 0)
+            {
+                System.out.println("foobar");
+            }
+            else if(i % 5 == 0)
+            {
+                System.out.println("foo");
+            }
+            else if(i % 3 == 0)
+            {
+                System.out.println("bar");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
+    }
 
 }
--- src/GameModel.java
+++ src/GameModel.java
@@ -2,5 +2,5 @@
  * Created by Luke on 10/9/2016.
  */
 public class GameModel {
+
 }
-//**raph is gay
